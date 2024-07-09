4276671c7654047bae2c12f4e2fc310ac637a4b4
sonhavietnamese <nguyensonha.hanz@gmail.com>
2024-07-09 10:33:35 +0700

    feat: scafold starter

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // output: 'export',
+  experimental: {
+    serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
+  },
+};
 
 export default nextConfig;
