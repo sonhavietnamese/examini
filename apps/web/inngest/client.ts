96a8f82ce89b28b0aa3d4b92de9911645446fa00
sonhavietnamese <nguyensonha.hanz@gmail.com>
2024-07-09 12:12:07 +0700

    fix(scaffold): deploy inngest

--- apps/web/inngest/client.ts
+++ apps/web/inngest/client.ts
@@ -1,4 +1,8 @@
 import { Inngest } from 'inngest'
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: 'my-app' })
+export const inngest = new Inngest({
+	id: 'my-app',
+	eventKey: process.env.INNGEST_EVENT_KEY,
+	baseUrl: process.env.INNGEST_BASE_URL,
+})
